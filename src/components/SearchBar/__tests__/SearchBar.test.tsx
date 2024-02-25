22a3e7405d525e17b4b38b599177674d24b5790f
mariano jimenez <marianojimenezperez1@gmail.com>
2024-02-25 17:35:36 +0100

    fix: test context provider fix

--- src/components/SearchBar/__tests__/SearchBar.test.tsx
+++ src/components/SearchBar/__tests__/SearchBar.test.tsx
@@ -1,18 +1,27 @@
 import { afterEach, describe, expect, it } from "vitest";
 import { cleanup, render, screen } from "@testing-library/react";
 import SearchBar from "../SearchBar";
+import { TodoProvider } from "@/context/TodoContext";
 
 describe("<SearchBar />", () => {
   afterEach(cleanup);
 
   it("Should render a form", () => {
-    render(<SearchBar />);
+    render(
+      <TodoProvider>
+        <SearchBar />
+      </TodoProvider>
+    );
 
     expect(screen.getByRole("form")).toBeDefined();
   });
 
   it("Should render a button width 'Add Todo' text", () => {
-    render(<SearchBar />);
+    render(
+      <TodoProvider>
+        <SearchBar />
+      </TodoProvider>
+    );
 
     expect(screen.getByText("Add Todo")).toBeDefined();
   });
